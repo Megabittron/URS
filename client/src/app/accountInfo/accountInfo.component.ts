4213486c0863138741dd476fafcfd0f9dffa81d6
twarling <warli010@morris.umn.edu>
2018-06-12 14:52:48 -0500

    Add missing options/imports to file #8

--- client/src/app/accountInfo/accountInfo.component.ts
+++ client/src/app/accountInfo/accountInfo.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
+import {AppService} from "../app.service";
+import {Router} from "@angular/router";
+import {Observable} from 'rxjs/Observable';
 
 @Component({
-    templateUrl: 'accountInfo.component.html'
+    selector: 'app-accountInfo-component',
+    templateUrl: 'accountInfo.component.html',
+    styleUrls: ['./accountInfo.component.css'],
+    providers: [AppService]
 })
 export class AccountInfoComponent {
     public text: string;
 
-    constructor() {
+    constructor(private router: Router) {
         this.text = 'Account Info';
     }
 }
