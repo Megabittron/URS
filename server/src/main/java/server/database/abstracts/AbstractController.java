9efe2a2e663321a1637ac3172c96cbebaa8ac69d
ahnafy <yasin011@morris.umn.edu>
2018-06-18 18:50:16 -0500

    add addnewAbstract function with all added fields

--- server/src/main/java/server/database/abstracts/AbstractController.java
+++ server/src/main/java/server/database/abstracts/AbstractController.java
@@ -9,6 +9,7 @@ import com.mongodb.util.JSON;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -98,12 +99,12 @@ public class AbstractController {
 
         }
 
-        if (queryParams.containsKey("abstract")) {
-            String targetContent = (queryParams.get("abstract")[0]);
+        if (queryParams.containsKey("abstracts")) {
+            String targetContent = (queryParams.get("abstracts")[0]);
             Document contentRegQuery = new Document();
             contentRegQuery.append("$regex", targetContent);
             contentRegQuery.append("$options", "i");
-            filterDoc = filterDoc.append("abstract", contentRegQuery);
+            filterDoc = filterDoc.append("abstracts", contentRegQuery);
         }
 
         if (queryParams.containsKey("presentationType")) {
@@ -174,13 +175,7 @@ public class AbstractController {
             contentRegQuery.append("$options", "i");
             filterDoc = filterDoc.append("cc", contentRegQuery);
         }
-        if (queryParams.containsKey("cc")) {
-            String targetContent = (queryParams.get("cc")[0]);
-            Document contentRegQuery = new Document();
-            contentRegQuery.append("$regex", targetContent);
-            contentRegQuery.append("$options", "i");
-            filterDoc = filterDoc.append("cc", contentRegQuery);
-        }
+
         if (queryParams.containsKey("rejection")) {
             String targetContent = (queryParams.get("rejection")[0]);
             Document contentRegQuery = new Document();
@@ -353,32 +348,87 @@ public class AbstractController {
             filterDoc = filterDoc.append("firstAdviserEmail", contentRegQuery);
         }
 
+        if (queryParams.containsKey("secondAdviserFirstName")) {
+            String targetContent = (queryParams.get("secondAdviserFirstName")[0]);
+            Document contentRegQuery = new Document();
+            contentRegQuery.append("$regex", targetContent);
+            contentRegQuery.append("$options", "i");
+            filterDoc = filterDoc.append("secondAdviserFirstName", contentRegQuery);
+        }
 
+        if (queryParams.containsKey("secondAdviserLastName")) {
+            String targetContent = (queryParams.get("secondAdviserLastName")[0]);
+            Document contentRegQuery = new Document();
+            contentRegQuery.append("$regex", targetContent);
+            contentRegQuery.append("$options", "i");
+            filterDoc = filterDoc.append("secondAdviserLastName", contentRegQuery);
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (queryParams.containsKey("secondAdviserEmail")) {
+            String targetContent = (queryParams.get("secondAdviserEmail")[0]);
+            Document contentRegQuery = new Document();
+            contentRegQuery.append("$regex", targetContent);
+            contentRegQuery.append("$options", "i");
+            filterDoc = filterDoc.append("secondAdviserEmail", contentRegQuery);
+        }
 
         //FindIterable comes from mongo, Document comes from Gson
         FindIterable<Document> matchingAbstracts = abstractCollection.find(filterDoc);
 
         return JSON.serialize(matchingAbstracts);
+
+    }
+/**
+    // Function that adds new Abstract to the Abstract collection
+    // with specific userID and automated time stamp
+*/
+    public String addNewAbstract(String userID, String title, String format, String abstracts, String presentationType, Boolean formatChange,
+                                 String discipline, Boolean featured, Boolean mediaServicesEquipment, String SpecialRequirements, String otherInfo,
+                                 Boolean approval, Boolean cc, Boolean rejection, Number group, String roomAssignment, Number totalRewriteVotes, Number majorRewriteVotes,
+                                 Number minorRewriteVotes, Number acceptedVotes, String comments, Boolean isPrimarySubmission, Boolean resubmitFlag, String firstPresenterFirstName,
+                                 String firstPresenterLastName, String firstPresenterEmail, String secondPresenterFirstName, String secondPresenterLastName,
+                                 String secondPresenterEmail, String thirdPresenterFirstName, String thirdPresenterLastName, String thirdPresenterEmail,
+                                 String firstAdviserFirstName, String firstAdviserLastName, String firstAdviserEmail, String secondAdviserFirstName,
+                                 String secondAdviserLastName, String secondAdviserEmail) {
+
+        Document newJournal = new Document();
+        newJournal.append("userID", userID);
+        newJournal.append("title", title);
+        newJournal.append("format", format);
+        newJournal.append("abstracts", abstracts);
+        newJournal.append("presentationType", presentationType);
+        newJournal.append("formatChange", formatChange);
+        newJournal.append("approval", approval);
+        newJournal.append("cc", cc);
+        newJournal.append("rejection", rejection);
+        newJournal.append("group", group);
+        newJournal.append("roomAssignment", roomAssignment);
+        newJournal.append("totalRewriteVotes", totalRewriteVotes);
+        newJournal.append("majorRewriteVotes", majorRewriteVotes);
+        newJournal.append("minorRewriteVotes", minorRewriteVotes);
+        newJournal.append("acceptedVotes", acceptedVotes);
+        newJournal.append("comments", comments);
+        newJournal.append("isPrimarySubmission", isPrimarySubmission);
+        newJournal.append("resubmitFlag", resubmitFlag);
+        newJournal.append("firstPresenterFirstName", firstPresenterFirstName);
+        newJournal.append("firstPresenterLastName", firstPresenterLastName);
+        newJournal.append("firstPresenterEmail", firstPresenterEmail);
+        newJournal.append("secondPresenterFirstName", secondPresenterFirstName);
+        newJournal.append("secondPresenterLastName", secondPresenterLastName);
+        newJournal.append("secondPresenterEmail", secondPresenterEmail);
+        newJournal.append("thirdPresenterFirstName", thirdPresenterFirstName);
+        newJournal.append("thirdPresenterLastName", thirdPresenterLastName);
+        newJournal.append("thirdPresenterEmail", thirdPresenterEmail);
+        newJournal.append("firstAdviserFirstName", firstAdviserFirstName);
+        newJournal.append("firstAdviserLastName", firstAdviserLastName);
+        newJournal.append("firstAdviserEmail", firstAdviserEmail);
+        newJournal.append("secondAdviserFirstName", secondAdviserFirstName);
+        newJournal.append("secondAdviserLastName", secondAdviserLastName);
+        newJournal.append("secondAdviserEmail", secondAdviserEmail);
+
+        Date timestamp = new Date();
+        newJournal.append("timestamp", timestamp.toString());
+
 
     }
 }
