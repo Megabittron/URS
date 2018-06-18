b26a379f5af59bdec07f6bdb4083e7d1b668155f
ahnafy <yasin011@morris.umn.edu>
2018-06-18 17:10:01 -0500

    initialize abstract collection controller

--- server/src/main/java/server/database/abstracts/AbstractController.java
+++ server/src/main/java/server/database/abstracts/AbstractController.java
@@ -1,19 +1,33 @@
 package server.database.abstracts;
 
 import com.google.gson.Gson;
-import com.mongodb.*;
+import com.mongodb.MongoException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
 import org.bson.Document;
 import org.bson.types.ObjectId;
+
 import java.util.Iterator;
 import java.util.Map;
-
-import java.util.Date;
 
 import static com.mongodb.client.model.Filters.eq;
 
 public class AbstractController {
+    private final Gson gson;
+    private MongoDatabase database;
+
+    // AbstractCollection is the collection that the collection data is in"
+
+    private final MongoCollection<Document> abstractCollection;
+
+    // Construct Abstract controller for the Abstracts:
+    public AbstractController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        abstractCollection = database.getCollection("abstracts");
+    }
+
+
 }
