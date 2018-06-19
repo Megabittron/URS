b2a187d729eeca3172c3bb1032ffaecbcc997fc9
ahnafy <yasin011@morris.umn.edu>
2018-06-19 14:51:57 -0500

    add getAbstractJSON function

--- server/src/main/java/server/database/abstracts/AbstractRequestHandler.java
+++ server/src/main/java/server/database/abstracts/AbstractRequestHandler.java
@@ -14,8 +14,46 @@ public class AbstractRequestHandler {
      *
      * @param abstractController
      */
+
     public AbstractRequestHandler(AbstractController abstractController){
         this.abstractController = abstractController;
+    }
+
+    /**
+    * Abstracts request handler to fetch abstracts in a Json Format
+    *
+    */
+
+ public String getAbstractJSON(Request req, Response res){
+        res.type("application/json");
+        String id = req.params("id");
+
+        // String variable was chosen as "abstracts" instead singular "abstract" to prevent java errors
+
+        String abstracts;
+        try {
+            abstracts = abstractController.getAbstract(id);
+        } catch (IllegalArgumentException e) {
+
+            // This is thrown if the ID doesn't have the appropriate
+            // form for a Mongo Object ID.
+            // https://docs.mongodb.com/manual/reference/method/ObjectId/
+
+            res.status(400);
+            res.body("The requested abstract id " + id + " wasn't a legal Mongo Object ID.\n" +
+                    "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
+            return "";
+        }
+
+     // String variable was chosen as "abstracts" instead singular "abstract" to prevent java errors
+
+     if (abstracts != null) {
+            return abstracts;
+        } else {
+            res.status(404);
+            res.body("The requested abstract with id " + id + " was not found");
+            return "";
+        }
     }
 
 
