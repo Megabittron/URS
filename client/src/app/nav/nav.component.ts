d02eb35a7bee2dd6b149f62f92c485f769d02fea
CMenne <menne019@morris.umn.edu>
2018-07-12 12:57:53 -0500

    Add click function to profile picture

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../authentication.service";
+import {User} from "../user";
 
 @Component({
     selector: 'app-nav',
     templateUrl: 'nav.component.html',
     styleUrls: ['nav.component.scss'],
-    providers: []
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
     public text: string;
 
-    constructor() {
+    public authIsLoaded: boolean = false;
+    public isLoggedIn: boolean = false;
+    public user: User;
+    public profilePic: string = null;
+    public infoState: string = 'inactive';
+
+    constructor(private authenticationService: AuthenticationService) {
         this.text = 'Nav';
     }
+    toggleInfoState() {
+        this.infoState = this.infoState === 'active' ? 'inactive' : 'active';
+    }
+
+    signOut(): void {
+        this.authenticationService.signOut();
+    }
+
+    ngOnInit() {
+        this.authenticationService.isLoaded$.subscribe( value => {
+            this.authIsLoaded = value;
+        });
+
+        this.authenticationService.isLoggedIn$.subscribe( value => {
+            this.isLoggedIn = value;
+        });
+
+        this.authenticationService.user$.subscribe(value => {
+            this.user = value;
+            this.profilePic = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
+        })
+
+    }
+
 }
